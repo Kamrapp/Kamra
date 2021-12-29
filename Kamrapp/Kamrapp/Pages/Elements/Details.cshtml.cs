efad1bb89c9ceb1e9f5ad954e5552fd7936b8e7b
Koala06 <barna.geller@gmail.com>
2021-12-29 18:14:55 +0100

    Base data structure (Element, Tag, Property) and bootstrap
    Added Element, Component, Tag, Property, PropertyValue objects and connections between them
    
    Changed Bootstrap style to dark
    
    # Conflicts:
    #	Kamrapp/Kamrapp/Data/ApplicationDbContext.cs
    #	Kamrapp/Kamrapp/Kamrapp.csproj
    #	Kamrapp/Kamrapp/Pages/Shared/_Layout.cshtml

---
+++ Kamrapp/Kamrapp/Pages/Elements/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Kamrapp.Data;
+using Kamrapp.Model;
+
+namespace Kamrapp.Pages.Elements
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Kamrapp.Data.ApplicationDbContext _context;
+
+        public DetailsModel(Kamrapp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Element Element { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Element = await _context.Elements.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Element == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Kamrapp/Kamrapp/Pages/Tags/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Kamrapp.Data;
+using Kamrapp.Model;
+
+namespace Kamrapp.Pages.Tags
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Kamrapp.Data.ApplicationDbContext _context;
+
+        public IndexModel(Kamrapp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Tag> Tag { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Tag = await _context.Tags.ToListAsync();
+        }
+    }
+}
