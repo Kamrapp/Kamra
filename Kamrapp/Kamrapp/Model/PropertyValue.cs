130d559a4edbd780f92c24270d65d5d7cdccdd29
Koala06 <barna.geller@gmail.com>
2021-12-29 18:14:55 +0100

    Base data structure (Element, Tag, Property) and bootstrap
    Added Element, Component, Tag, Property, PropertyValue objects and connections between them
    
    Changed Bootstrap style to dark
    
    # Conflicts:
    #	Kamrapp/Kamrapp/Data/ApplicationDbContext.cs
    #	Kamrapp/Kamrapp/Kamrapp.csproj
    #	Kamrapp/Kamrapp/Pages/Shared/_Layout.cshtml

---
+++ Kamrapp/Kamrapp/Model/PropertyValue.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kamrapp.Model
+{
+    public class PropertyValue
+    {
+        public PropertyValue()
+        {
+            Type = EType.Unset;
+        }
+        public enum EType
+        {
+            Unset,
+            Bool,
+            String,
+            Int,
+            Double
+        }
+
+        [Required]
+        [ForeignKey(nameof(Property))]
+        public int PropertyId { get; set; }
+        public virtual Property Property { get; set; }
+
+
+        public EType Type
+        {
+            get;
+            /*
+                {
+                    return
+                        Bool != null ? EType.Bool
+                      : String != null ? EType.String
+                      : Int != null ? EType.Int
+                      : Double != null ? EType.Double
+                      : EType.Unset;
+                }
+            */
+            private set;
+            /*
+                { 
+                }
+            */
+        }
+
+        public object? Value
+        {
+            get
+            {
+                switch (Type)
+                {
+                    case EType.Unset:
+                        return null;
+                    case EType.Bool:
+                        return Bool;
+                    case EType.String:
+                        return String;
+                    case EType.Int:
+                        return Int;
+                    case EType.Double:
+                        return Double;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+
+        #region Variable value members
+        bool? _bool = null;
+        public bool? Bool
+        {
+            get { return _bool; }
+            set { _bool = value; Type = EType.Bool; }
+        }
+
+        string? _string = null;
+        public string? String
+        {
+            get => _string;
+            set { _string = value; Type = EType.String; }
+        }
+
+        int? _int = null;
+        public int? Int
+        {
+            get { return _int; }
+            set { _int = value; Type = EType.Int; }
+        }
+
+        double? _double = null;
+        public double? Double
+        {
+            get { return _double; }
+            set { _double = value; Type = EType.Double; }
+        }
+        #endregion Variable value members
+
+    }
+}
---
+++ Kamrapp/Kamrapp/Model/Tag2Tag.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kamrapp.Model
+{
+    public class Tag2Tag
+    {
+        public Tag2Tag()
+        {
+            PropertyValues = new List<PropertyValue>();
+        }
+
+        [Required]
+        [ForeignKey(nameof(ParentTag))]
+        public int ParentTagId { get; set; }
+        public virtual Tag ParentTag { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(ChildTag))]
+        public int ChildTagId { get; set; }
+        public virtual Tag ChildTag { get; set; }
+
+
+        [Range(-10000, 10000,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public double Weight { get; set; }
+
+        // TODO: PropertyValues need to Reference the Properties inherited from Tags and their respective Values as well...
+        public ICollection<PropertyValue> PropertyValues { get; set; }
+
+    }
+}
