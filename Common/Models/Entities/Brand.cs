9d954d63ab60ab9e440fa52c91cefe067533e186
legrab <barna.geller@gmail.com>
2023-04-26 18:14:45 +0200

    add Brand entity

---
+++ Common/Models/Entities/Brand.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.Entities
+{
+    public class Brand
+    {
+        public Brand()
+        {
+            Stocks = new List<Stock>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Stock> Stocks { get; set; }
+
+        //TODO Brands should have Stores that have Addresses
+        //public Address Address { get; set; }
+
+    }
+
+}
