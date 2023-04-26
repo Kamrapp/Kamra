3092aed3969c7be09349bd79156cede182af7436
legrab <barna.geller@gmail.com>
2023-04-26 21:12:35 +0200

    random

---
+++ Fetcher/Instance/Fetcher.cs
@@ -0,0 +1,50 @@
+﻿using Models.Records.Lidl;
+
+namespace Fetcher.Instance
+{
+    public class Fetcher
+    {
+        private IComboLogger Logger { get; set; }
+
+        private const string LogCollection = "";
+        private const string LidlCollection = "";
+        private const string AldiCollection = "";
+        private static string LogPath => $"KamraFetcher\\Logs\\Fetcher\\log_{DateTime.Now:yyyy_MM_dd__hh_mm_ss}.txt";
+
+        private IBaseRecordRepository<LidlProduct> lidlRepository;
+
+
+        public Fetcher()
+        {
+            Logger = new ComboLogger(LogPath, LogCollection);
+
+        }
+
+        public async Task Fetch()
+        {
+            await InitFetch();
+
+            Logger.Log(LoggerType.Console, LogType.Info, $"========================================================");
+            Logger.Log(LoggerType.Console, LogType.Info, $"      Fetching started at:  {DateTime.Now}");
+            Logger.Log(LoggerType.Console, LogType.Info, $"========================================================");
+            Logger.Log(LogType.Info, $"Fetching started");
+
+        }
+
+        public async Task InitFetch()
+        {
+            Logger.Log(LoggerType.File, LogType.Debug, $"Initializing Fetcher...");
+
+            InitDatabase();
+
+            Logger.Log(LoggerType.File, LogType.Debug, $"Initialization successful.");
+        }
+
+        private void InitDatabase()
+        {
+            var database = MongoDbConnector.MongoDbConnector.InitDatabase();
+
+            Logger.SetConnection(database);
+        }
+    }
+}
---
+++ Fetcher/Usings.cs
@@ -0,0 +1,5 @@
+﻿global using MongoDbConnector.Log;
+global using MongoDbConnector.Repository.Interfaces;
+
+global using Shared.Records.Lidl;
+global using Shared.Utils.Logger;
