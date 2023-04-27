a41269683c838304295ccf5d797fdb3fb9318890
legrab <barna.geller@gmail.com>
2023-04-27 19:44:21 +0200

    add migration for fetched data to Element

---
+++ KamraBE/DataAccess/Migrations/20230427174358_Element fetch data.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class Elementfetchdata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Elements",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Distributor",
+                table: "Elements",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MongoId",
+                table: "Elements",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUri",
+                table: "Elements",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Elements",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Elements");
+
+            migrationBuilder.DropColumn(
+                name: "Distributor",
+                table: "Elements");
+
+            migrationBuilder.DropColumn(
+                name: "MongoId",
+                table: "Elements");
+
+            migrationBuilder.DropColumn(
+                name: "PictureUri",
+                table: "Elements");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Elements");
+        }
+    }
+}
